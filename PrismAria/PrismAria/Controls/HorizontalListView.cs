3aecb57b134c0cab0516a030ffa48120bc413fd8
k-francisco <piattosnovalays@gmail.com>
2017-10-09 01:41:01 +0800

    fixed the fonts for ios and fixed the user page for ios tab

--- PrismAria/PrismAria/Controls/HorizontalListView.cs
+++ PrismAria/PrismAria/Controls/HorizontalListView.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
 using System.Text;
+using System.Windows.Input;
 using Xamarin.Forms;
 
 namespace PrismAria.Controls
@@ -27,7 +31,58 @@ namespace PrismAria.Controls
             set { SetValue(ItemTemplateProperty, value); }
         }
 
+        private static object HandleBindingPropertyChangedDelegate(BindableObject bindable, object value) {
+            throw new NotImplementedException();
+        }
 
+        
+        public event EventHandler<ItemTappedEventArgs> ItemSelected;
+
+        public static readonly BindableProperty SelectedCommandProperty =
+            BindableProperty.Create("SelectedCommand", typeof(ICommand), typeof(HorizontalListView), null);
+
+        public ICommand SelectedCommand
+        {
+            get { return (ICommand)GetValue(SelectedCommandProperty); }
+            set { SetValue(SelectedCommandProperty, value); }
+        }
+   
+
+        public static readonly BindableProperty SelectedCommandParameterProperty =
+            BindableProperty.Create("SelectedCommandParameter", typeof(object), typeof(HorizontalListView), null);
+
+        public object SelectedCommandParameter
+        {
+            get { return GetValue(SelectedCommandParameterProperty); }
+            set { SetValue(SelectedCommandParameterProperty, value); }
+        }
+
+        static void HandleBindingPropertyChangedDelegate(BindableObject bindable, object oldValue, object newValue)
+        {
+            var isOldObservable = oldValue?.GetType().GetTypeInfo().ImplementedInterfaces.Any(i => i == typeof(INotifyCollectionChanged));
+            var isNewObservable = newValue?.GetType().GetTypeInfo().ImplementedInterfaces.Any(i => i == typeof(INotifyCollectionChanged));
+
+            var tl = (HorizontalListView)bindable;
+            if (isOldObservable.GetValueOrDefault(false))
+            {
+                ((INotifyCollectionChanged)oldValue).CollectionChanged -= tl.HandleCollectionChanged;
+            }
+
+            if (isNewObservable.GetValueOrDefault(false))
+            {
+                ((INotifyCollectionChanged)newValue).CollectionChanged += tl.HandleCollectionChanged;
+            }
+
+            if (oldValue != newValue)
+            {
+                tl.Render();
+            }
+        }
+
+        private void HandleCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            Render();
+        }
 
         public void Render()
         {
@@ -39,8 +94,22 @@ namespace PrismAria.Controls
 
             foreach (var item in this.ItemSource)
             {
+                var command = SelectedCommand ?? new Command((obj) =>
+                {
+                    var args = new ItemTappedEventArgs(ItemSource, item);
+                    ItemSelected?.Invoke(this, args);
+                });
+                var commandParameter = SelectedCommandParameter ?? item;
+
                 var viewCell = this.ItemTemplate.CreateContent() as ViewCell;
                 viewCell.View.BindingContext = item;
+                viewCell.View.GestureRecognizers.Add(new TapGestureRecognizer
+                {
+                    Command = command,
+                    CommandParameter = commandParameter,
+                    NumberOfTapsRequired = 1
+                });
+
                 layout.Children.Add(viewCell.View);
             }
 
